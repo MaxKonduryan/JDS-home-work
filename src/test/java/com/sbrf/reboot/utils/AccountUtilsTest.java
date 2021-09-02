c030cab38d5d7b23b4984729300e30a351171c1a
MaxKonduryan <Max.Kondor@gmail.com>
2021-09-02 23:15:57 +0300

    Create Account DTO and AccountUtils with test

--- src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
+++ src/test/java/com/sbrf/reboot/utils/AccountUtilsTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 class AccountUtilsTest {
@@ -45,5 +44,26 @@ class AccountUtilsTest {
         Assertions.assertEquals(2L, accounts.get(1).getId());
         Assertions.assertEquals(LocalDate.now().minusDays(3), accounts.get(2).getCreateDate());
         Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(3).getCreateDate());
+    }
+
+    @Test
+    void sortedByIdDateBal() {
+        List<Account> accounts = new ArrayList<Account>() {{
+            add(Account.builder().id(1L).createDate(LocalDate.now().minusDays(4)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.valueOf(560000L, 2)).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.valueOf(17)).build());
+            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.TEN).build());
+            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
+        }};
+
+        AccountUtils.sortedByIdDateBal(accounts);
+
+        Assertions.assertEquals(1L, accounts.get(0).getId());
+        Assertions.assertEquals(2L, accounts.get(1).getId());
+        Assertions.assertEquals(BigDecimal.TEN, accounts.get(2).getBalance());
+        Assertions.assertEquals(BigDecimal.valueOf(17), accounts.get(3).getBalance());
+        Assertions.assertEquals(BigDecimal.valueOf(560000L, 2), accounts.get(4).getBalance());
+        Assertions.assertEquals(LocalDate.now().minusDays(1), accounts.get(5).getCreateDate());
     }
 }
